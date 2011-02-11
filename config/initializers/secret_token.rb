098df111487b914a0955f9c20e6f7fd4a029d422
jim <jimiray@gmail.com>
2011-02-11 10:54:27 -0700

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Growapair::Application.config.secret_token = '5e1a00854c7de22172c6c868e8c2a4b2b3b5b740578d86b0e43cf5414b05e8b14965f6d749d87b096bbfcc3c66f540a97da97f8f25a9e0279a4d7131f8e278ff'
